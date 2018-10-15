ca5ca94e93b94a736a2ed3d8ecd3e80a8a86c5b8
ktheu <khtheuer@t-online.de>
2018-10-15 16:30:17 +0200

    functionen

---
+++ .ipynb_checkpoints/mytools-checkpoint.py
@@ -0,0 +1,15 @@
+def func1(x):
+    '''
+    x: float
+    returns: den Wert der Funktion x*x - 4
+    '''
+    return x*x-4
+
+
+def func2(x, y):
+    '''
+    x: float
+    returns: den Wert x*x + y*y
+    '''
+    return x*x + y*y
+    
--- myprog.py
+++
@@ -1,5 +0,0 @@
-import mytools
-
-a = mytools.func1(4)
-b = mytools.func2(3,6)
-print(a,b)
